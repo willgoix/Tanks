0158398799d8f47aa6a67793d1fe51a3276b35c9
Willian Gois <willian2o12@Hotmail.com>
2018-11-30 15:50:16 -0200

    Updates

--- menu.py
+++ menu.py
@@ -4,246 +4,263 @@ from functools import partial
 
 
 class Menu(ui.UI):
-	def __init__(self, screen):
-		ui.UI.__init__(self, screen)
+    def __init__(self, screen):
+        ui.UI.__init__(self, screen)
 
-		self.logo = label.Image([my.SCREEN_HALF_WIDTH, -my.SCREEN_HEIGHT], pygame.image.load("assets/logo.png"),
-								centralization=ui.BOTTOM)
-		self.addWidget(self.logo)
+        self.logo = label.Image([my.SCREEN_HALF_WIDTH, -my.SCREEN_HEIGHT], pygame.image.load("assets/logo.png"),
+                                centralization=ui.BOTTOM)
+        self.addWidget(self.logo)
 
-		self.playOfflineButton = button.ImageButton(lambda: self.playOffline(),
-													[my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
-													text="Jogar Offline",
-													image_surface=ui.IMAGES['button'],
-													image_pressed=ui.IMAGES['button_pressed'], )
-		self.playOnlineButton = button.ImageButton(lambda: self.playOnline(),
-												   [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
-												   text="Jogar Online",
-												   image_surface=ui.IMAGES['button'],
-												   image_pressed=ui.IMAGES['button_pressed'])
-		self.optionsButton = button.ImageButton(lambda: self.options(),
-												[my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
-												text="Opções",
-												image_surface=ui.IMAGES['button'],
-												image_pressed=ui.IMAGES['button_pressed'])
-		self.creditsButton = button.ImageButton(lambda: self.credits(),
-												[my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
-												text="Créditos",
-												image_surface=ui.IMAGES['button'],
-												image_pressed=ui.IMAGES['button_pressed'])
-		self.addWidget(self.playOfflineButton)
-		self.addWidget(self.playOnlineButton)
-		self.addWidget(self.optionsButton)
-		self.addWidget(self.creditsButton)
-		self.addWidget(label.Text([my.SCREEN_WIDTH - 5, my.SCREEN_HEIGHT], "Tanks v" + my.VERSION, 20,
-								  centralization=ui.LEFT | ui.UPPER))
+        self.playOfflineButton = button.ImageButton(lambda: self.playOffline(),
+                                                    [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
+                                                    text="Jogar Offline",
+                                                    image_surface=ui.IMAGES['button'],
+                                                    image_pressed=ui.IMAGES['button_pressed'], )
+        self.playOnlineButton = button.ImageButton(lambda: self.playOnline(),
+                                                   [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
+                                                   text="Jogar Online",
+                                                   image_surface=ui.IMAGES['button'],
+                                                   image_pressed=ui.IMAGES['button_pressed'])
+        self.optionsButton = button.ImageButton(lambda: self.options(),
+                                                [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
+                                                text="Opções",
+                                                image_surface=ui.IMAGES['button'],
+                                                image_pressed=ui.IMAGES['button_pressed'])
+        self.creditsButton = button.ImageButton(lambda: self.credits(),
+                                                [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT],
+                                                text="Créditos",
+                                                image_surface=ui.IMAGES['button'],
+                                                image_pressed=ui.IMAGES['button_pressed'])
+        self.addWidget(self.playOfflineButton)
+        self.addWidget(self.playOnlineButton)
+        self.addWidget(self.optionsButton)
+        self.addWidget(self.creditsButton)
+        self.addWidget(label.Text([my.SCREEN_WIDTH - 5, my.SCREEN_HEIGHT], "Tanks v" + my.VERSION, 20,
+                                  centralization=ui.LEFT | ui.UPPER))
 
-		self.animation = True
-		self.readyToNext = False
-		self.next = self
+        self.animation = True
+        self.readyToNext = False
+        self.next = self
 
-	def update(self, events):
-		# self.screen.blit(ui.IMAGES['background'], (0, 0))
-		self.screen.fill(my.RED)
-		super().update(events)
+    def update(self, events):
+        # self.screen.blit(ui.IMAGES['background'], (0, 0))
+        self.screen.fill(my.RED)
+        super().update(events)
 
-		if self.animation:
-			# TO UP
-			y = 0
-			for button in (self.playOfflineButton, self.playOnlineButton, self.optionsButton, self.creditsButton):
-				if button.pos[1] > my.SCREEN_HALF_HEIGHT + y:
-					button.pos[1] -= int(math.fabs(my.SCREEN_HALF_HEIGHT + y - button.pos[1]) * 0.1)
-					y += 60
+        if self.animation:
+            # TO UP
+            y = 0
+            for button in (self.playOfflineButton, self.playOnlineButton, self.optionsButton, self.creditsButton):
+                if button.pos[1] > my.SCREEN_HALF_HEIGHT + y:
+                    button.pos[1] -= int(math.fabs(my.SCREEN_HALF_HEIGHT + y - button.pos[1]) * 0.1)
+                    y += 60
 
-			# TO DOWN
-			if self.logo.pos[1] < 0:
-				self.logo.pos[1] += math.fabs(self.logo.pos[1]) * 0.1
-		else:
-			# TO UP
-			y = 0
-			for button in (self.playOfflineButton, self.playOnlineButton, self.optionsButton, self.creditsButton):
-				if button.pos[1] < my.SCREEN_HEIGHT:
-					button.pos[1] += (my.SCREEN_HEIGHT + y - button.pos[1]) * 0.1
-					y += 60
+            # TO DOWN
+            if self.logo.pos[1] < 0:
+                self.logo.pos[1] += math.fabs(self.logo.pos[1]) * 0.1
+        else:
+            # TO UP
+            y = 0
+            for button in (self.playOfflineButton, self.playOnlineButton, self.optionsButton, self.creditsButton):
+                if button.pos[1] < my.SCREEN_HEIGHT:
+                    button.pos[1] += (my.SCREEN_HEIGHT + y - button.pos[1]) * 0.1
+                    y += 60
 
-			# TO DOWN
-			if self.logo.pos[1] < 0:
-				self.logo.pos[1] += math.fabs(self.logo.pos[1]) * 0.1
+            # TO DOWN
+            if self.logo.pos[1] < 0:
+                self.logo.pos[1] += math.fabs(self.logo.pos[1]) * 0.1
 
-		return self.next
+        return self.next
 
-	def playOffline(self):
-		sound.play('click')
-		self.animation = False
-		self.next = PlayOfflineMenu(self.screen)
+    def playOffline(self):
+        sound.play('click')
+        self.animation = False
+        self.next = PlayOfflineMenu(self.screen)
 
-	def playOnline(self):
-		sound.play('click')
-		self.animation = False
-		self.next = PlayOnlineMenu(self.screen)
+    def playOnline(self):
+        sound.play('click')
+        self.animation = False
+        self.next = PlayOnlineMenu(self.screen)
 
-	def options(self):
-		sound.play('click')
-		self.animation = False
-		self.next = OptionsMenu(self.screen)
+    def options(self):
+        sound.play('click')
+        self.animation = False
+        self.next = OptionsMenu(self.screen)
 
-	def credits(self):
-		sound.play('click')
-		self.animation = False
-		self.next = CreditsMenu(self.screen)
+    def credits(self):
+        sound.play('click')
+        self.animation = False
+        self.next = CreditsMenu(self.screen)
 
 
 class PlayOfflineMenu(ui.UI):
+    def __init__(self, screen):
+        ui.UI.__init__(self, screen)
 
-	def __init__(self, screen):
-		ui.UI.__init__(self, screen)
+        self.addWidget(label.Text([50, 50], "Configure o jogo:", fontsize=18, centralization=ui.RIGHT))
 
-		self.addWidget(label.Text(my.SCREEN_HALF_SIZE, "Em desenvolvimento...", fontsize=24))
-		self.addWidget(button.ImageButton(lambda: self.back(),
-										  [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT - 100],
-										  text="Voltar",
-										  image_surface=ui.IMAGES['button'],
-										  image_pressed=ui.IMAGES['button_pressed']))
+        self.enemiesValue = label.Text([my.SCREEN_HALF_WIDTH + 50, 65], "5", fontsize=18)
+        self.addWidget(self.enemiesValue)
+        self.addWidget(label.Text([my.SCREEN_HALF_WIDTH - 10, 65], "Inimigos", fontsize=18))
+        self.addWidget(
+            slider.SliderBar(partial(self.sliderEnemies), [my.SCREEN_HALF_WIDTH, 100], (100, 5), min=1, max=10, step=1,
+                             initial=5,
+                             image_slider=ui.IMAGES['slider'],
+                             image_pointer=ui.IMAGES['slider_pointer']))
 
-		self.next = self
+        self.addWidget(button.ImageButton(lambda: self.back(),
+                                          [my.SCREEN_HALF_WIDTH - 20, my.SCREEN_HEIGHT - 100],
+                                          text="Voltar",
+                                          image_surface=ui.IMAGES['button'],
+                                          image_pressed=ui.IMAGES['button_pressed'],
+                                          centralization=ui.LEFT))
+        self.addWidget(button.ImageButton(lambda: self.startGame(),
+                                          [my.SCREEN_HALF_WIDTH + 20, my.SCREEN_HEIGHT - 100],
+                                          text="Iniciar Jogo",
+                                          image_surface=ui.IMAGES['button_green'],
+                                          image_pressed=ui.IMAGES['button_green_pressed'],
+                                          centralization=ui.RIGHT))
 
-	def update(self, events):
-		self.screen.fill(my.RED)
-		super().update(events)
+        self.next = self
 
-		return self.next
+    def update(self, events):
+        self.screen.fill(my.RED)
+        super().update(events)
 
-	def back(self):
-		self.next = Menu(self.screen)
+        return self.next
 
-	def select(self, content):
-		print(content, " selected")
+    def sliderEnemies(self, value):
+        self.enemiesValue.text = str(value)
+
+    def back(self):
+        self.next = Menu(self.screen)
+
+    def startGame(self):
+        print("start")
 
 
 class PlayOnlineMenu(ui.UI):
+    def __init__(self, screen):
+        ui.UI.__init__(self, screen)
 
-	def __init__(self, screen):
-		ui.UI.__init__(self, screen)
+        self.addWidget(label.Text(my.SCREEN_HALF_SIZE, "Em desenvolvimento...", fontsize=24))
+        self.addWidget(button.ImageButton(lambda: self.back(),
+                                          [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT - 100],
+                                          text="Voltar",
+                                          image_surface=ui.IMAGES['button'],
+                                          image_pressed=ui.IMAGES['button_pressed']))
 
-		self.addWidget(label.Text(my.SCREEN_HALF_SIZE, "Em desenvolvimento...", fontsize=24))
-		self.addWidget(button.ImageButton(lambda: self.back(),
-										  [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT - 100],
-										  text="Voltar",
-										  image_surface=ui.IMAGES['button'],
-										  image_pressed=ui.IMAGES['button_pressed']))
+        self.next = self
 
-		self.next = self
+    def update(self, events):
+        self.screen.fill(my.RED)
+        super().update(events)
 
-	def update(self, events):
-		self.screen.fill(my.RED)
-		super().update(events)
+        return self.next
 
-		return self.next
-
-	def back(self):
-		self.next = Menu(self.screen)
+    def back(self):
+        self.next = Menu(self.screen)
 
 
 class OptionsMenu(ui.UI):
+    def __init__(self, screen):
+        ui.UI.__init__(self, screen)
 
-	def __init__(self, screen):
-		ui.UI.__init__(self, screen)
+        self.fpsValue = label.Text([my.SCREEN_HALF_WIDTH + 25, 60], str(my.FPS), fontsize=20)
+        self.addWidget(self.fpsValue)
+        self.addWidget(label.Text([my.SCREEN_HALF_WIDTH - 25, 60], "FPS", fontsize=20))
+        self.addWidget(
+            slider.SliderBar(partial(self.sliderFps), [my.SCREEN_HALF_WIDTH, 100], (300, 5), min=20, max=100, step=1,
+                             initial=my.FPS,
+                             image_slider=ui.IMAGES['slider'],
+                             image_pointer=ui.IMAGES['slider_pointer']))
 
-		self.fpsValue = label.Text([my.SCREEN_HALF_WIDTH + 25, 60], str(my.FPS), fontsize=20)
-		self.addWidget(self.fpsValue)
-		self.addWidget(label.Text([my.SCREEN_HALF_WIDTH - 25, 60], "FPS", fontsize=20))
-		self.addWidget(
-			slider.SliderBar(partial(self.sliderFps), [my.SCREEN_HALF_WIDTH, 100], (300, 5), min=20, max=100, step=1,
-							 initial=my.FPS,
-							 image_slider=ui.IMAGES['slider'],
-							 image_pointer=ui.IMAGES['slider_pointer']))
+        self.addWidget(button.Drop(self, ['FULLSCREEN', '1920 x 1080', '1280 x 1080', '960 x 680', '660 x 480'],
+                                   partial(self.selectResolution),
+                                   [my.SCREEN_HALF_WIDTH, 100],
+                                   text="Resolução",
+                                   image_surface=ui.IMAGES['button'],
+                                   image_pressed=ui.IMAGES['button_pressed'],
+                                   down=True))
 
-		self.addWidget(button.Drop(self, ['FULLSCREEN', '1920 x 1080', '1280 x 1080', '960 x 680', '660 x 480'], partial(self.selectResolution),
-								   [my.SCREEN_HALF_WIDTH, 100],
-								   text="Resolução",
-								   image_surface=ui.IMAGES['button'],
-								   image_pressed=ui.IMAGES['button_pressed'],
-								   down=True))
+        self.addWidget(button.ImageButton(lambda: self.back(),
+                                          [my.SCREEN_HALF_WIDTH - 20, my.SCREEN_HEIGHT - 100],
+                                          text="Voltar",
+                                          image_surface=ui.IMAGES['button'],
+                                          image_pressed=ui.IMAGES['button_pressed'],
+                                          centralization=ui.LEFT))
+        self.addWidget(button.ImageButton(lambda: self.save(),
+                                          [my.SCREEN_HALF_WIDTH + 20, my.SCREEN_HEIGHT - 100],
+                                          text="Salvar",
+                                          image_surface=ui.IMAGES['button_green'],
+                                          image_pressed=ui.IMAGES['button_green_pressed'],
+                                          centralization=ui.RIGHT))
+        self.next = self
 
-		self.addWidget(button.ImageButton(lambda: self.back(),
-										  [my.SCREEN_HALF_WIDTH - 20, my.SCREEN_HEIGHT - 100],
-										  text="Voltar",
-										  image_surface=ui.IMAGES['button'],
-										  image_pressed=ui.IMAGES['button_pressed'],
-										  centralization=ui.LEFT))
-		self.addWidget(button.ImageButton(lambda: self.save(),
-										  [my.SCREEN_HALF_WIDTH + 20, my.SCREEN_HEIGHT - 100],
-										  text="Salvar",
-										  image_surface=ui.IMAGES['button_green'],
-										  image_pressed=ui.IMAGES['button_green_pressed'],
-										  centralization=ui.RIGHT))
-		self.next = self
+    def update(self, events):
+        self.screen.fill(my.RED)
+        super().update(events)
 
-	def update(self, events):
-		self.screen.fill(my.RED)
-		super().update(events)
+        return self.next
 
-		return self.next
+    def sliderFps(self, value):
+        self.fpsValue.text = str(value)
+        my.CONFIG.set('fps', value)
 
-	def sliderFps(self, value):
-		self.fpsValue.text = str(value)
-		my.CONFIG.set('fps', value)
+    def selectResolution(self, value):
+        if value == 'FULLSCREEN':
+            my.CONFIG.set('windowed', False)
+        else:
+            my.CONFIG.set('windowed', True)
+            my.CONFIG.set('screen_width', int(value.split(" x ")[0]))
+            my.CONFIG.set('screen_height', int(value.split(" x ")[1]))
 
-	def selectResolution(self, value):
-		if value == 'FULLSCREEN':
-			my.CONFIG.set('windowed', False)
-		else:
-			my.CONFIG.set('windowed', True)
-			my.CONFIG.set('screen_width', int(value.split(" x ")[0]))
-			my.CONFIG.set('screen_height', int(value.split(" x ")[1]))
+    def back(self):
+        self.next = Menu(self.screen)
 
-	def back(self):
-		self.next = Menu(self.screen)
+    def save(self):
+        self.next = Menu(self.screen)
+        my.FPS = my.CONFIG.get('fps')
 
-	def save(self):
-		self.next = Menu(self.screen)
-		my.FPS = my.CONFIG.get('fps')
+        oldwidth = my.SCREEN_WIDTH
+        my.WINDOWED = my.CONFIG.get('windowed')
+        my.SCREEN_WIDTH = my.CONFIG.get('screen_width')
+        my.SCREEN_HEIGHT = my.CONFIG.get('screen_height')
+        my.SCREEN_HALF_WIDTH = my.CONFIG.get('screen_width') / 2
+        my.SCREEN_HALF_HEIGHT = my.CONFIG.get('screen_height') / 2
+        my.SCREEN_SIZE = (my.SCREEN_WIDTH, my.SCREEN_HEIGHT)
+        my.SCREEN_HALF_SIZE = (my.SCREEN_HALF_WIDTH, my.SCREEN_HALF_HEIGHT)
+        if my.CONFIG.get('screen_width') != oldwidth:
+            my.ENGINE.adaptScreen()
 
-		oldwidth = my.SCREEN_WIDTH
-		my.WINDOWED = my.CONFIG.get('windowed')
-		my.SCREEN_WIDTH = my.CONFIG.get('screen_width')
-		my.SCREEN_HEIGHT = my.CONFIG.get('screen_height')
-		my.SCREEN_HALF_WIDTH = my.CONFIG.get('screen_width') / 2
-		my.SCREEN_HALF_HEIGHT = my.CONFIG.get('screen_height') / 2
-		my.SCREEN_SIZE = (my.SCREEN_WIDTH, my.SCREEN_HEIGHT)
-		my.SCREEN_HALF_SIZE = (my.SCREEN_HALF_WIDTH, my.SCREEN_HALF_HEIGHT)
-		if my.CONFIG.get('screen_width') != oldwidth:
-			my.ENGINE.adaptScreen()
-
-		my.CONFIG.save()
+        my.CONFIG.save()
 
 
 class CreditsMenu(ui.UI):
+    def __init__(self, screen):
+        ui.UI.__init__(self, screen)
 
-	def __init__(self, screen):
-		ui.UI.__init__(self, screen)
+        self.addWidget(label.Text([200, 100], "Desenvolvido por:", fontsize=24, centralization=ui.RIGHT))
+        self.addWidget(label.Text([250, 140], "Willian Gois ~the coder guy", fontsize=20, centralization=ui.RIGHT))
+        self.addWidget(label.Text([250, 160], "Lucas Verona ~the h4ck3r m4n", fontsize=20, centralization=ui.RIGHT))
 
-		self.addWidget(label.Text([200, 100], "Desenvolvido por:", fontsize=24, centralization=ui.RIGHT))
-		self.addWidget(label.Text([250, 140], "Willian Gois ~the coder guy", fontsize=20, centralization=ui.RIGHT))
-		self.addWidget(label.Text([250, 160], "Lucas Verona ~the h4ck3r m4n", fontsize=20, centralization=ui.RIGHT))
+        self.addWidget(label.Text([200, 210], "Texturas por:", fontsize=24, centralization=ui.RIGHT))
+        self.addWidget(
+            label.Text([250, 250], "(UI): Kenney Vleugels (www.kenney.nl)", fontsize=20, centralization=ui.RIGHT))
 
-		self.addWidget(label.Text([200, 210], "Texturas por:", fontsize=24, centralization=ui.RIGHT))
-		self.addWidget(
-			label.Text([250, 250], "(UI): Kenney Vleugels (www.kenney.nl)", fontsize=20, centralization=ui.RIGHT))
+        self.addWidget(button.ImageButton(lambda: self.back(),
+                                          [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT - 100],
+                                          text="Voltar",
+                                          image_surface=ui.IMAGES['button'],
+                                          image_pressed=ui.IMAGES['button_pressed']))
 
-		self.addWidget(button.ImageButton(lambda: self.back(),
-										  [my.SCREEN_HALF_WIDTH, my.SCREEN_HEIGHT - 100],
-										  text="Voltar",
-										  image_surface=ui.IMAGES['button'],
-										  image_pressed=ui.IMAGES['button_pressed']))
+        self.next = self
 
-		self.next = self
+    def update(self, events):
+        self.screen.fill(my.RED)
+        super().update(events)
 
-	def update(self, events):
-		self.screen.fill(my.RED)
-		super().update(events)
+        return self.next
 
-		return self.next
-
-	def back(self):
-		self.next = Menu(self.screen)
+    def back(self):
+        self.next = Menu(self.screen)
